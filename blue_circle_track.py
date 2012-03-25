c674654980f1977cfc7c01c29df6d3e6128c9ae1
Kan O <zkan.cs@gmail.com>
2012-03-25 23:01:56 +0700

    implementing a program for tracking blue circles, and it has bugs in creating new tracks

---
+++ blue_circle_track.py
@@ -0,0 +1,156 @@
+from SimpleCV import *
+import time
+import numpy
+import operator
+
+def get_overlap_area(minx1, miny1, maxx1, maxy1, minx2, miny2, maxx2, maxy2):
+    if minx1 > maxx2 or maxx1 < minx2 or miny1 > maxy2 or maxy1 < miny2:
+        return 0
+    else:
+        return (min(maxx2, maxx1) - max(minx2, minx1)) * (min(maxy2, maxy1) - max(miny2, miny1))
+
+class Track:
+    def __init__(self):
+        self.ID = 0
+        self.blobs = []
+        self.matched_blob_list = []
+
+cam = Camera(0)
+
+tracks = []
+track_id = 0
+overlap_th = 160
+
+while True:
+    img = cam.getImage()
+
+    blue_stuff = img.colorDistance(Color.BLUE)
+    bin_blue_stuff = blue_stuff.binarize(130)
+    clean_bin_blue_stuff = bin_blue_stuff.morphOpen().morphClose()
+
+    blobs = clean_bin_blue_stuff.findBlobs()
+
+    if blobs:
+        print 'num of blobs: ' + str(len(blobs))
+        if not tracks:
+            for b in blobs:
+                if b.isCircle(0.3) and b.area() > 100:
+                    t = Track()
+                    track_id = track_id + 1
+                    t.ID = track_id
+                    t.blobs.append(b)
+                    tracks.append(t)
+        
+        else:
+            matrix = numpy.zeros((len(tracks), len(blobs)))
+            for i in range(len(tracks)):
+                last_blob_idx = len(tracks[i].blobs) - 1
+                for j in range(len(blobs)):
+                    matrix[i][j] = get_overlap_area(
+                        tracks[i].blobs[last_blob_idx].minX(), tracks[i].blobs[last_blob_idx].minY(),
+                        tracks[i].blobs[last_blob_idx].maxX(), tracks[i].blobs[last_blob_idx].maxY(),
+                        blobs[j].minX(), blobs[j].minY(),
+                        blobs[j].maxX(), blobs[j].maxY())
+
+            for i in range(len(tracks)):
+                matched_blob_list = []
+                for j in range(len(blobs)):
+                    if matrix[i][j] >= overlap_th:
+                        matched_blob_list.append(j)
+
+                if len(matched_blob_list) == 1:
+                    tracks[i].matched_blob_list.append(j)
+            
+            unmatched_blob_list = []
+            for j in range(len(blobs)):
+                matched_track_list = []
+                num_zero = 0
+                for i in range(len(tracks)):
+                    if matrix[i][j] >= overlap_th:
+                        matched_track_list.append(i)
+                    elif matrix[i][j] == 0:
+                        num_zero = num_zero + 1
+
+                if len(matched_track_list) == 1:
+                    if j in tracks[matched_track_list[0]].matched_blob_list:
+                        tracks[matched_track_list[0]].blobs.append(blobs[j])
+#                        blobs.pop(j)
+
+                if num_zero == len(tracks):
+                    unmatched_blob_list.append(j)
+
+            # create new tracks for unmatched blobs
+            for idx in unmatched_blob_list:
+                if blobs[idx].isCircle(0.3) and blobs[idx].area() > 100:
+                    t = Track()
+                    track_id = track_id + 1
+                    t.ID = track_id
+                    t.blobs.append(blobs[idx])
+                    tracks.append(t)
+
+    for t in tracks:
+        last_blob_idx = len(t.blobs) - 1
+        layer = DrawingLayer((img.width, img.height))
+        rect_dim = (t.blobs[last_blob_idx].width(), 
+            t.blobs[last_blob_idx].height())
+        center = t.blobs[last_blob_idx].center()
+        layer.centeredRectangle(center, rect_dim, Color.RED)
+        img.addDrawingLayer(layer)
+    
+    img.applyLayers()
+    img.show()
+
+    time.sleep(0.1)
+
+#    print blue_stuff.getNumpy()
+
+#    blue_stuff = img.hueDistance(Color.BLUE).show()
+
+#    blobs = img.findBlobs((0, 0, 100))
+
+#    [circle.drawHull(color = Color.RED, width = 10) for circle in blobs.filter([b.isCircle() for b in blobs])]
+'''
+    clist = []
+    for b in blobs:
+        if b.isCircle():
+            clist.append(b)
+
+#    blue_stuff = img.colorDistancie(Color.BLUE)
+#    blobs = blue_stuff.findBlobs()
+
+#    blue_stuff.show()
+
+    for b in blobs:
+        layer = DrawingLayer((img.width, img.height))
+        rect_dim = (b.width(), b.height())
+        center = b.center()
+        layer.centeredRectangle(center, rect_dim, Color.RED)
+        img.addDrawingLayer(layer)
+    
+    img.applyLayers()
+    img.show()
+'''
+'''
+    # remove noise then find blobs in the clean image
+    blobs = img.morphOpen().morphClose().findBlobs(10)
+
+
+    for t in tracks:
+        last_blob_idx = len(t.blobs) - 1
+        layer = DrawingLayer((img.width, img.height))
+        rect_dim = (t.blobs[last_blob_idx].width(), t.blobs[last_blob_idx].height())
+        center = t.blobs[last_blob_idx].center()
+        layer.centeredRectangle(center, rect_dim, t.color)
+        img.addDrawingLayer(layer)
+    
+    img.applyLayers()
+    img.show()
+
+    raw_input()
+
+    img.clearLayers()
+    
+    time.sleep(1)
+'''
+
+
--- track.py
+++ track.py
@@ -4,34 +4,28 @@ import time
 import numpy
 import operator
 
-class Track:
-    def __init__(self):
-        self.ID = 0
-        self.center = (0, 0)
-        self.minX = 0
-        self.maxX = 0
-        self.minY = 0
-        self.maxY = 0
-        self.width = 0
-        self.height = 0
-        self.color = (0, 0, 0)
-        mark_as_match = False
-
 def get_overlap_area(minx1, miny1, maxx1, maxy1, minx2, miny2, maxx2, maxy2):
     if minx1 > maxx2 or maxx1 < minx2 or miny1 > maxy2 or maxy1 < miny2:
         return 0
     else:
         return (min(maxx2, maxx1) - max(minx2, minx1)) * (min(maxy2, maxy1) - max(miny2, miny1))
 
-def get_color_similarity(hue1, hue2):
+def get_color_dissimilarity(hue1, hue2):
     return abs(hue1 - hue2)
+
+class Track:
+    def __init__(self):
+        self.ID = 0
+        self.color = (0, 0, 0)
+        self.blobs = []
+        self.mark_as_match = False
 
 # sort files by name because the resulting list is in arbitrary order.
 files = sorted(glob.glob('images/*.jpg'))
 
-# list of tracks
-track_list = []
+tracks = []
 track_id = 0
+overlap_th = 160
 
 for file in files:
     img = Image(file)
@@ -41,124 +35,116 @@ for file in files:
 
     # if there are blobs detected
     if blobs:
-        if not track_list:
+        if not tracks:
             for b in blobs:
                 t = Track()
                 track_id = track_id + 1
                 t.ID = track_id
-                # throw out the remainder
-                # meanColor() returns BGR space, so need to reverse to RGB
-                t.color = tuple([int(b.meanColor()) 
-                    if isinstance(b.meanColor(), float) else int(x) 
-                    for x in reversed(b.meanColor())])
-                t.center = b.center()
-                t.minX = b.minX()
-                t.maxX = b.maxX()
-                t.minY = b.minY()
-                t.maxY = b.maxY()
-                t.width = b.maxX() - b.minX()
-                t.height = b.maxY() - b.minY()
-                track_list.append(t)
+                # meanColor() returns BGR space, so need to reverse to RGB (also, throw out the remainder)
+                t.color = tuple([int(b.meanColor()) if isinstance(b.meanColor(), float) else int(x) for x in reversed(b.meanColor())])
+                t.blobs.append(b)
+                tracks.append(t)
 
         else:
-            matrix = numpy.zeros((len(track_list), len(blobs)))
-            for i in range(len(track_list)):
+            matrix = numpy.zeros((len(tracks), len(blobs)))
+            for i in range(len(tracks)):
+                last_blob_idx = len(tracks[i].blobs) - 1
                 for j in range(len(blobs)):
                     # create a correspondence matrix
-                    print 'minX1: ' + str(track_list[i].minX)
-                    print 'maxX1: ' + str(track_list[i].maxX)
-                    print 'minY1: ' + str(track_list[i].minY)
-                    print 'maxY1: ' + str(track_list[i].maxY)
+                   # print 'minX1: ' + str(track_list[i].minX)
+                   # print 'maxX1: ' + str(track_list[i].maxX)
+                   # print 'minY1: ' + str(track_list[i].minY)
+                   # print 'maxY1: ' + str(track_list[i].maxY)
                          
-                    print 'minX2: ' + str(blobs[j].minX())
-                    print 'maxX2: ' + str(blobs[j].maxX())
-                    print 'minY2: ' + str(blobs[j].minY())
-                    print 'maxY2: ' + str(blobs[j].maxY())
+                   # print 'minX2: ' + str(blobs[j].minX())
+                   # print 'maxX2: ' + str(blobs[j].maxX())
+                   # print 'minY2: ' + str(blobs[j].minY())
+                   # print 'maxY2: ' + str(blobs[j].maxY())
                     
                     matrix[i][j] = get_overlap_area(
-                        track_list[i].minX, track_list[i].minY,
-                        track_list[i].maxX, track_list[i].maxY,
+                        tracks[i].blobs[last_blob_idx].minX(), tracks[i].blobs[last_blob_idx].minY(),
+                        tracks[i].blobs[last_blob_idx].maxX(), tracks[i].blobs[last_blob_idx].maxY(),
                         blobs[j].minX(), blobs[j].minY(), 
                         blobs[j].maxX(), blobs[j].maxY())
-
-                    print matrix[i][j]
             
             print 'matrix:'
-            for i in range(len(track_list)):
+            for i in range(len(tracks)):
                 for j in range(len(blobs)):
                     print str(matrix[i][j]),
                 print '\n'
 
-            for i in range(len(track_list)):
-                print 'processing on track ' + str(i)
-                matched_blob_idx = []
-                track_list[i].mark_as_match = False
+            removed_blob_list = []
+            for i in range(len(tracks)):
+                matched_blob_list = []
+
                 for j in range(len(blobs)):
-                    if matrix[i][j] >= 100:
-                        matched_blob_idx.append(j)
+                    if matrix[i][j] >= overlap_th:
+                        matched_blob_list.append(j)
 
                 # splitting
-                if len(matched_blob_idx) > 1:
-                    print 'blob splitting' + str(matched_blob_idx)
+                if len(matched_blob_list) > 1:
+                    print 'blob splitting' + str(matched_blob_list)
+
+                    # get track's color
                     px = Image((1, 1), ColorSpace.RGB)
-                    px[0, 0] = track_list[i].color
+                    px[0, 0] = tracks[i].color
                     pxHSV = px.toHSV()
                     hue1 = pxHSV[0, 0][2]
+#                    print 'track ' + str(i) + ': ' + str(hue1)
 
-                    print 'track ' + str(i) + ': ' + str(hue1)
                     min_diff = 10000
-                    best_match_idx = -1
-                    for idx in matched_blob_idx:
-                        px[0, 0] = blobs[idx].meanColor()
+                    best_bob_match = -1
+                    best_track_match = -1
+                    for bid in matched_blob_list:
+                        if bid in removed_blob_list:
+                            continue
+
+                        px[0, 0] = blobs[bid].meanColor()
                         pxHSV = px.toHSV()
                         hue2 = pxHSV[0, 0][2]
+#                        print 'blob ' + str(bid) + ': ' + str(hue2)
 
-                        color_similarity = get_color_similarity(hue1, hue2)
-                        print 'blob ' + str(idx) + ': ' + str(hue2)
-                        print 'track ' + str(i) + ' and blob ' + str(idx) + ': ' + str(color_similarity)
+                        color_dissimilarity = get_color_dissimilarity(hue1, hue2)
 
-                        if min_diff > color_similarity:
-                            min_diff = color_similarity
-                            best_match_idx = idx
+                        if color_dissimilarity == 0:
+                            print 'track ' + str(i) + ' matches blob ' + str(bid) + ': ' + str(color_dissimilarity)
 
-                    print 'best macth for track: ' + str(i) + ' is ' + str(best_match_idx)
-                    if not track_list[i].mark_as_match:
-                        track_list[i].center = blobs[best_match_idx].center()
-                        track_list[i].minX = blobs[best_match_idx].minX()
-                        track_list[i].maxX = blobs[best_match_idx].maxX()
-                        track_list[i].minY = blobs[best_match_idx].minY()
-                        track_list[i].maxY = blobs[best_match_idx].maxY()
-                        track_list[i].mark_as_match = True
+                            tracks[i].blobs.append(blobs[bid])
+                            removed_blob_list.append(bid)
+                        else:
+                            tracks[i].blobs.append(blobs[bid])
+                            removed_blob_list.append(bid)
 
-                elif len(matched_blob_idx) == 1:
-                    if not track_list[i].mark_as_match:
-                        track_list[i].mark_as_match = True
+#                            if min_diff > color_similarity:
+#                                min_diff = color_similarity
+#                                best_blob_match = bid
+#                                best_track_match = tid
 
+#                    print 'best match for track: ' + str(best_track_match) + ' is ' + str(best_blob_match)
+
+#                    if not tracks[best_track_match].mark_as_match:
+#                        tracks[best_track_match].blobs.append(blobs[best_blob_match])
+#                        tracks[best_track_match].mark_as_match = True
+
+                elif len(matched_blob_list) == 1:
+                    if not tracks[i].mark_as_match:
+                        tracks[i].mark_as_match = True
+                        
             for j in range(len(blobs)):
                 matched_track_idx = []
-                for i in range(len(track_list)):
-                    if matrix[i][j] >= 100:
-                        matched_track_idx.append(i)
+                for i in range(len(tracks)):
+                    if matrix[i][j] >= overlap_th:
+                        matched_track_idx.append(i) 
 
                 # merging (only center is updated)
                 if len(matched_track_idx) > 1:
                     print 'tracks merging: ' + str(matched_track_idx)
                     for idx in matched_track_idx:
-                        track_list[idx].center = tuple(map(operator.div, 
-                            tuple(map(operator.add, track_list[idx].center, 
-                            blobs[j].center())), (2, 2)))
-#                        track_list[idx].minX = track_list[idx].minX + (track_list[idx].center[0] + blobs[j].center()[0])/2
-#                        track_list[idx].maxX = track_list[idx].maxX + (track_list[idx].center[0] + blobs[j].center()[0])/2
-#                        track_list[idx].minY = track_list[idx].minY + (track_list[idx].center[1] + blobs[j].center()[1])/2
-#                        track_list[idx].maxY = track_list[idx].maxY + (track_list[idx].center[1] + blobs[j].center()[1])/2
+                        tracks[idx].blobs.append(blobs[j])
 
                 elif len(matched_track_idx) == 1:
-                    if track_list[matched_track_idx[0]].mark_as_match: 
-                        track_list[matched_track_idx[0]].center = blobs[j].center()
-                        track_list[matched_track_idx[0]].minX = blobs[j].minX()
-                        track_list[matched_track_idx[0]].maxX = blobs[j].maxX()
-                        track_list[matched_track_idx[0]].minY = blobs[j].minY()
-                        track_list[matched_track_idx[0]].maxY = blobs[j].maxY()
+                    if tracks[matched_track_idx[0]].mark_as_match: 
+                        tracks[matched_track_idx[0]].blobs.append(blobs[j])
 
         for b in blobs:    
             layer = DrawingLayer((img.width, img.height))
@@ -168,15 +154,16 @@ for file in files:
             layer.centeredRectangle(center, rect_dim, Color.WHITE)
             img.addDrawingLayer(layer)
 
-    for t in track_list:
-        print t.center
+    for t in tracks:
+        last_blob_idx = len(t.blobs) - 1
+        print t.blobs[last_blob_idx].center()
 
-    for i in range(len(track_list)):
+    for t in tracks:
+        last_blob_idx = len(t.blobs) - 1
         layer = DrawingLayer((img.width, img.height))
-        rect_dim = (track_list[i].maxX - track_list[i].minX,
-            track_list[i].maxY - track_list[i].minY)
-        center = track_list[i].center
-        layer.centeredRectangle(center, rect_dim, track_list[i].color)
+        rect_dim = (t.blobs[last_blob_idx].width(), t.blobs[last_blob_idx].height())
+        center = t.blobs[last_blob_idx].center()
+        layer.centeredRectangle(center, rect_dim, t.color)
         img.addDrawingLayer(layer)
     
     img.applyLayers()
